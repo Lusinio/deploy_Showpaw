51d6d25aa0b19051f7b18f797883abd8b3e23b6f
JrSem <juniorsemerzier@gmail.com>
2020-08-27 12:56:52 -0400

    Updates

---
+++ precache-manifest.e9bc66de24b7da5c2d33411b03d33adb.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7fffd1da28a188688a1b054d3f9b097",
+    "url": "/deploy_Showpaw/index.html"
+  },
+  {
+    "revision": "f21a79c6bc518a287f12",
+    "url": "/deploy_Showpaw/static/css/main.37fdbf24.chunk.css"
+  },
+  {
+    "revision": "5084bacb86701d89d80a",
+    "url": "/deploy_Showpaw/static/js/2.ae690a0e.chunk.js"
+  },
+  {
+    "revision": "f21a79c6bc518a287f12",
+    "url": "/deploy_Showpaw/static/js/main.587170ee.chunk.js"
+  },
+  {
+    "revision": "626f3c5d1e0ba229ecc0",
+    "url": "/deploy_Showpaw/static/js/runtime-main.9c3223ba.js"
+  },
+  {
+    "revision": "bc5d19f68206e154229ffe50289e8863",
+    "url": "/deploy_Showpaw/static/media/Belo.bc5d19f6.jpeg"
+  },
+  {
+    "revision": "4298c9485ea87317a2fd38344b5d755e",
+    "url": "/deploy_Showpaw/static/media/Bic.4298c948.jpeg"
+  },
+  {
+    "revision": "e83315a33a7f63ad8f0b500c03ecb74a",
+    "url": "/deploy_Showpaw/static/media/Desca.e83315a3.jpeg"
+  },
+  {
+    "revision": "d0139f150aa10a473bb1080e48ca33ee",
+    "url": "/deploy_Showpaw/static/media/Ekip.d0139f15.jpeg"
+  },
+  {
+    "revision": "fa3a6fad24d3fe4229884afd29e1e5dd",
+    "url": "/deploy_Showpaw/static/media/Enposib.fa3a6fad.jpeg"
+  },
+  {
+    "revision": "6888fccc67f7252f08847caf4ca34856",
+    "url": "/deploy_Showpaw/static/media/Fantom.6888fccc.jpeg"
+  },
+  {
+    "revision": "2347795c48ce4cb04c1513b0371ef389",
+    "url": "/deploy_Showpaw/static/media/Kadilak.2347795c.jpeg"
+  },
+  {
+    "revision": "c90bbd2a629252f92a36e6623ac9895e",
+    "url": "/deploy_Showpaw/static/media/Klass1.c90bbd2a.jpeg"
+  },
+  {
+    "revision": "687be7f67280a0499173089ce73eab3d",
+    "url": "/deploy_Showpaw/static/media/Konpakreyol.687be7f6.jpeg"
+  },
+  {
+    "revision": "ba6df386ffd0c21f2fb2313f2ca4761b",
+    "url": "/deploy_Showpaw/static/media/Kreyolla.ba6df386.jpeg"
+  },
+  {
+    "revision": "cc520467b86e463062b7b6f48a67e722",
+    "url": "/deploy_Showpaw/static/media/defaultBcg.cc520467.jpg"
+  },
+  {
+    "revision": "5b97ffe20018219ba61e3400999ae409",
+    "url": "/deploy_Showpaw/static/media/header-bg.5b97ffe2.jpg"
+  },
+  {
+    "revision": "a2b923b1b0c8de3dbc10f52412ae59cc",
+    "url": "/deploy_Showpaw/static/media/micky.a2b923b1.jpeg"
+  },
+  {
+    "revision": "e29bb0398b671970add5ab49ab21b551",
+    "url": "/deploy_Showpaw/static/media/wyclef.e29bb039.jpeg"
+  }
+]);
